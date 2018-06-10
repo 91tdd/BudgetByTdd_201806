a5ae47961a2d49689800547594057aebc78799ef
hatelove <jay.hatelove@gmail.com>
2018-06-10 14:54:25 +0800

    pass test: no budgets

---
+++ BudgetByTdd/Accounting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BudgetByTdd
+{
+    public class Accounting
+    {
+        private readonly IBudgetRepository _budgetRepository;
+
+        public Accounting(IBudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+}
--- BudgetByTdd/UnitTest1.cs
+++ BudgetByTdd/UnitTest1.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
 
 namespace BudgetByTdd
 {
     [TestClass]
-    public class UnitTest1
+    public class AccountingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void no_budgets()
         {
+            var budgetRepository = Substitute.For<IBudgetRepository>();
+            var accounting = new Accounting(budgetRepository);
+            var start = new DateTime(2018, 6, 1);
+            var end = new DateTime(2018, 6, 1);
+            var totalAmount = accounting.TotalAmount(start, end);
+            Assert.AreEqual(0m, totalAmount);
         }
     }
 }
